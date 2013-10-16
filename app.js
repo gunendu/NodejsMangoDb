2e025a90888c2b0261dadff19143742b709066a0
gunendu <gunendu@gunendu-Inspiron-N5010.(none)>
2013-10-16 23:28:01 +0530

    init

--- app.js
+++ app.js
@@ -38,19 +38,55 @@ app.post('/Source', function(req, res) {
    var content = req.body.Content;
    var startdate = req.body.StartDate;
    var enddate = req.body.EndDate;
+   var key;
+   var values =  [];
+   var tbl_row = "";
+   var tmp = 1;
+   var date = "Date";
+   var title = "Title";
+   var source = "Source";
+   var content = "Content";
+
    if(val.length>1) {
    searchData.sourceList(val,function(err,sourceList){
-   res.render('display',{title:"Filter by source",entries:sourceList});
+   for(key in sourceList){
+        if(sourceList.hasOwnProperty(key)){
+   	
+   tbl_row+= "<table border=" +tmp+ "> <tr> <th>" +date+ "</th> <th>" +title+ "</th> <th>" +source+ "</th> <th>" +content+ "</th> </tr> <tr> <td>" +sourceList[key].date+ "</td><td>" +sourceList[key].title +"</td><td>" +sourceList[key].source+ "</td><td>" +sourceList[key].content+ "</td></tr> </table";
+   }
+   }
+   res.writeHeader(200, {"Content-Type": "text/html"});  
+   res.write(tbl_row);  
+   res.end();  	
   });
 }
-  if(title.length>1 || content.length>1) {
+  else if(title.length>1 && content.length>1) {
   searchData.func2(title,content,function(err,func2){
-  res.render('display',{title:"Regex Data over Title and source",entries:func2});
+
+  for(key in func2){
+        if(func2.hasOwnProperty(key)){
+   
+   tbl_row+= "<table border=" +tmp+ "> <tr> <th>" +date+ "</th> <th>" +title+ "</th> <th>" +source+ "</th> <th>" +content+ "</th> </tr> <tr> <td>" +func2[key].date+ "</td><td>" +func2[key].title +"</td><td>" +func2[key].source+ "</td><td>" +func2[key].content+ "</td></tr> </table";
+   }
+   }
+   res.writeHeader(200, {"Content-Type": "text/html"});
+   res.write(tbl_row);
+   res.end();
+
 });
 }
-  if(startdate.length>1 && enddate.length>1){
+  else {
   searchData.func3(startdate,enddate,function(err,func3){
-  res.render('display',{title:"Filter by Date",entries:func3});
+  for(key in func3){
+        if(func3.hasOwnProperty(key)){
+
+   tbl_row+= "<table border=" +tmp+ "> <tr> <th>" +date+ "</th> <th>" +title+ "</th> <th>" +source+ "</th> <th>" +content+ "</th> </tr> <tr> <td>" +func3[key].date+ "</td><td>" +func3[key].title +"</td><td>" +func3[key].source+ "</td><td>" +func3[key].content+ "</td></tr> </table";
+   }
+   }
+   res.writeHeader(200, {"Content-Type": "text/html"});
+   res.write(tbl_row);
+   res.end();
+
 });
 }
 
